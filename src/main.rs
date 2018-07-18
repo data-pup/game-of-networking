3b6e7a7cbb84cf0a752b5ebcbfbe7dd57c9e00b1
data-pup <kevinmartinucr@gmail.com>
2018-07-18 17:04:50 -0400

    Added some external crates for the first steps in this.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate tokio;
+
+use std::net::{
+    IpAddr,
+    Ipv4Addr,
+    // SocketAddr
+};
+
+// use tokio::io;
+// use tokio::net::TcpListener;
+// use tokio::prelude::*;
+
+static _PORT_RANGE_BASE: u32 = 5000;
+
+lazy_static! {
+    static ref _LOCALHOST: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+}
+
 fn main() {
     println!("Hello, world!");
 }
